56a48e2631116047fbeb70063b37dac7b11e37c9
Kenneth Jones <admin@techhandie.com>
2020-07-13 23:29:25 -0400

    revert back to the use of if statements using the actual html code for each element in the condition

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var eventObjects = document.querySelectorAll(".drum");//array of elements with t
 var buttonLimit = eventObjects.length;
 var soundUrl = "";
 
-function playSound(index) {
-  var soundUrl = "";
-  var sounds = ["sounds/tom-1.mp3","sounds/tom-2.mp3","sounds/tom-3.mp3","sounds/tom-4.mp3","sounds/crash.mp3","sounds/kick-bass.mp3","sounds/snare.mp3"];
-
-  soundUrl = sounds(index);
+function playSound(soundUrl) {
+  //var soundUrl = "";
+  //var sounds = ["sounds/tom-1.mp3","sounds/tom-2.mp3","sounds/tom-3.mp3","sounds/tom-4.mp3","sounds/crash.mp3","sounds/kick-bass.mp3","sounds/snare.mp3"];
+  //soundUrl = sounds(index);
+  
   var sound = new Audio(soundUrl);//sounds
   sound.play();
 }
@@ -22,8 +22,37 @@ for ( var i = 0; i < buttonLimit; i++ ) {
   var currObj = eventObjects[i];
   var index = i;
   console.log(`index: ${index}\n\relement: ${currObj}`);
+
   //Check for div moniker on current element
   //Set sound file
+
+if ( currObj === '<button class=​"w drum">​w​</button>'​
+ ) { soundUrl = "sounds/tom-1.mp3"; }//
+
+
+if ( currObj === '<button class=​"a drum">​a</button>'​
+ ) { soundUrl = "sounds/tom-2.mp3"; }//
+
+
+if (  currObj === '<button class=​"s drum">​s</button>'​
+  ) { soundUrl = "sounds/tom-3.mp3"; }//
+
+
+if ( currObj === '<button class=​"d drum">​d</button>'​
+ ) { soundUrl = "sounds/tom-4.mp3"; }//
+
+
+if ( currObj === '<button class=​"j drum">​j​</button>'​
+ ) { soundUrl = "sounds/crash.mp3"; }//
+
+
+if ( currObj === '<button class=​"k drum">​k​</button>'​
+ ) { soundUrl = "sounds/kick-bass.mp3"; }//
+
+
+if ( currObj === '<button class=​"l drum">​l​</button>'​
+ ) { soundUrl = "sounds/snare.mp3"; }//
+
 
   //Listen for click on cuurent element
   currObj.addEventListener("click",handleClick);
