42be02fce0511c0c0de6153f190d1c7e8a3a69bf
Kenneth Jones <admin@techhandie.com>
2020-07-16 23:04:28 -0400

    change all button letters to white after sound click

--- index2.js
+++ index2.js
@@ -18,31 +18,37 @@ for ( var i = 0; i < keyLimit; i++ ) {
 		case "a":
 			var tom2 = new Audio("sounds/tom-2.mp3");
 			tom2.play();
+      this.style.color = "white";
 			break;
 
 		case "s":
 			var tom3 = new Audio("sounds/tom-3.mp3");
 			tom3.play();
+      this.style.color = "white";
 			break;
 
 		case "d":
 			var tom4 = new Audio("sounds/tom-4.mp3");
 			tom4.play();
+      this.style.color = "white";
 			break;
 
 		case "j":
 			var crash = new Audio("sounds/crash.mp3");
 			crash.play();
+      this.style.color = "white";
 			break;
 
 		case "k":
 			var kickBass = new Audio("sounds/kick-bass.mp3");
 			kickBass.play();
+      this.style.color = "white";
 			break;
 
 		case "l":
 			var snare = new Audio("sounds/snare.mp3");
 			snare.play();
+      this.style.color = "white";
 			break;
 
         default:
