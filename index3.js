41075c0b47a6c597a9d304a2168a83a06981ba10
Kenneth Jones <admin@techhandie.com>
2020-07-19 00:34:43 -0400

    remive semicolon from the sounds object and replace with comma

--- index3.js
+++ index3.js
@@ -2,13 +2,13 @@ function playSound(char) {
 
 	var sound = '';
 	var sounds = {
-		"w": "sounds/tom-1.mp3";
-		"a": "sounds/tom-2.mp3";
-		"s": "sounds/tom-3.mp3";
-		"d": "sounds/tom-4.mp3";
-		"j": "sounds/crash.mp3";
-		"k": "sounds/kick-bass.mp3";
-		"l": "sounds/snare.mp3";
+		"w": "sounds/tom-1.mp3",
+		"a": "sounds/tom-2.mp3",
+		"s": "sounds/tom-3.mp3",
+		"d": "sounds/tom-4.mp3",
+		"j": "sounds/crash.mp3",
+		"k": "sounds/kick-bass.mp3",
+		"l": "sounds/snare.mp3"
 	}
 
 	if sounds.includes(char) { sound = sounds.char };
