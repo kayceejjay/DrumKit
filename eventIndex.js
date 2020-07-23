0dafccfa6d6c034e042781696e21934a2d22093f
Kenneth Jones <admin@techhandie.com>
2020-07-22 23:14:09 -0400

    eventIndex.js - refactoring with animation

---
+++ eventIndex.js
@@ -0,0 +1,60 @@
+//
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for ( i=0; i < numberOfDrumButtons; i++ ) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    playSound(buttonInnerHTML);
+
+  });
+
+}//for
+
+
+  document.querySelectorAll(".drum")[i].addEventListener("keydown",function(event) {
+
+    playSound(event.key);
+
+  });
+
+  function playSound() {
+
+    switch (char) {
+
+      case "w":
+        playSound(char);
+        break;
+
+      case "a":
+        playSound(char);
+        break;
+
+      case "s":
+        playSound(char);
+        break;
+
+      case "d":
+        playSound(char);
+        break;
+
+      case "j":
+        playSound(char);
+        break;
+
+      case "k":
+        playSound(char);
+        break;
+
+      case "l":
+        playSound(char);
+        break;
+
+      default:
+        console.log(`No ${char} HTML element.`);
+
+    } //switch
+
+  }//playSound
